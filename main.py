6b25a618ad4470b6ae9557bc74f4e7913313d2a6
Shehma Ali <shehmaali08@gmail.com>
2026-06-17 23:50:24 +0500

    goals refined to be better

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+TODO
+1: clean CSVs by cities and countries side by side
+2: set up graphs designs
+3: set up the web application by streamlit
+4: ship the project
+
+TOTAL countries and cites = 10 countries and 10 cites
+"""
+
+
