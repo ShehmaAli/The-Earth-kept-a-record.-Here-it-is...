654febfe97f95e921f9a5442731f6d176adb1857
Shehma Ali <shehmaali08@gmail.com>
2026-06-29 01:11:22 +0500

    feat: added a new function to give every single country as a list

--- data_clean.py
+++ data_clean.py
@@ -1,36 +1,55 @@
 import numpy as np
 import pandas as pd
+import streamlit as st
 
 
-# checking for any empty values in each of the CSVs
-# All the selected countries in the world
-world_data = pd.read_csv("Weather station csv/World_data.csv")
+@st.cache_data
+def load_data():
+    # checking for any empty values in each of the CSVs
+    # All the selected countries in the world
+    world_data = pd.read_csv("Weather station csv/World_data.csv")
 
-# CSVs of the selected cities
-multan = pd.read_csv("Weather station csv/multan.csv")
-delhi = pd.read_csv("Weather station csv/delhi.csv")
-london = pd.read_csv("Weather station csv/london.csv")
-new_york = pd.read_csv("Weather station csv/new york.csv")
-beijing = pd.read_csv("Weather station csv/beijing.csv")
-sydney = pd.read_csv("Weather station csv/sydney.csv")
-tokyo = pd.read_csv("Weather station csv/tokyo.csv")
-paris = pd.read_csv("Weather station csv/paris.csv")
-dubai = pd.read_csv("Weather station csv/dubai.csv")
-nuuk = pd.read_csv("Weather station csv/nuuk.csv")
+    # CSVs of the selected cities
+    multan = pd.read_csv("Weather station csv/multan.csv")
+    delhi = pd.read_csv("Weather station csv/delhi.csv")
+    london = pd.read_csv("Weather station csv/london.csv")
+    new_york = pd.read_csv("Weather station csv/new york.csv")
+    beijing = pd.read_csv("Weather station csv/beijing.csv")
+    sydney = pd.read_csv("Weather station csv/sydney.csv")
+    tokyo = pd.read_csv("Weather station csv/tokyo.csv")
+    paris = pd.read_csv("Weather station csv/paris.csv")
+    dubai = pd.read_csv("Weather station csv/dubai.csv")
+    nuuk = pd.read_csv("Weather station csv/nuuk.csv")
+
+    # another list from which the user can choose the graph for cities from
+    all_cities = {"Multan": multan,
+                  "Delhi": delhi,
+                  "London": london,
+                  "New york": new_york,
+                  "Beijing": beijing,
+                  "Sydney": sydney,
+                  "Tokyo": tokyo,
+                  "Paris": paris,
+                  "Dubai": dubai,
+                  "Nuuk": nuuk}
+
+    return world_data, all_cities
 
 
-# another list from which the user can choose the graph for cities from
-All_cities = {"multan": multan,
-              "delhi": delhi,
-              "london": london,
-              "new york": new_york,
-              "beijing": beijing,
-              "sydney":  sydney,
-              "tokyo": tokyo,
-              "paris": paris,
-              "dubai": dubai,
-              "nuuk": nuuk}
+def clean_csvs(all_cities, world_data):
+    for i in all_cities.values():
+        i.replace(999.90, np.nan, inplace=True)
+        i["metANN"] = i["metANN"].interpolate(method="linear")
+    world_data["Code"] = world_data["Code"].fillna("0")
+    return all_cities, world_data
 
-for i in All_cities.values():
-    i.replace(999.90, np.nan, inplace=True)
-    i["metANN"] = i["metANN"].interpolate(method="linear")
+
+def one_country_name(data):
+    country = ""
+    countries = []
+    for single_country in data["Entity"]:
+        if single_country != country:
+            countries.append(single_country)
+            country = single_country
+
+    return countries
