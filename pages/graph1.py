f0220f21f1b9922b2e76de6a767e82c0d582bf54
Shehma Ali <shehmaali08@gmail.com>
2026-07-08 00:16:23 +0500

    feat: individual city/county temperature trend graph done

--- pages/graph1.py
+++ pages/graph1.py
@@ -1,76 +1,53 @@
 import streamlit as st
 import plotly.express as px
-import plotly.graph_objects as go
+from data_clean import load_data, clean_csvs, one_country_name
 
-All_cities_str = ["multan", "delhi", "london", "new york", "beijing", "sydney", "tokyo", "paris", "dubai", "nuuk"]
+world_data, all_cities = load_data()
+Allcities, clean_world_data = clean_csvs(all_cities, world_data)
+
+
+def graph1(type, csv, name):
+    if type == "City":
+        years = csv["YEAR"]
+        temps = csv["metANN"]
+        final_title = f"Average annual temperature in {name}"
+    if type == "Country":
+        years = csv["Year"]
+        temps = csv["Temperature anomaly"]
+        final_title = f"Temperature anomalies in {name}"
+
+    fig = px.line(csv, x=years, y=temps, title= final_title)
+
+    return fig
 
 
 st.title("Welcome to Graph 1 :party:")
 
 st.markdown("## Choose the following")
 
+choices = ["Choose amoung the following:", "City", "Country"]
 
-left_column, right_column = st.columns(2, gap="medium", vertical_alignment="bottom")
+answer = st.radio("Choose: ", choices)
 
-with left_column:
-    city = st.button("City")
-    if city:
-        city_choosen = st.selectbox("City", All_cities_str)
+if answer == "City":
+    city_choosen = st.selectbox("City", ["Select any option"] + list(Allcities.keys()))
+    if city_choosen != "Select any option":
+        fig = graph1(answer, all_cities[city_choosen], city_choosen)
+        fig.update_layout(
+            margin=dict(l=50, r=20, t=40, b=40),
+            autosize=True
+        )
+        fig.update_xaxes(tickangle=45)
+        st.plotly_chart(fig, use_container_width=True)
 
-with right_column:
-    country = st.button("Country")
-    if country:
+if answer == "Country":
 
-
-
-
-
-def graph1(csv, name):
-    years = csv["YEAR"]
-    temps = csv["metANN"]
-
-    fig = go.Figure()
-
-    # plotting many small lines for gradient
-    n = len(years)
-
-    for i in range(n - 1):
-        t = i / (n - 2)
-        r = int(133 + t * (226 - 133))
-        g = int(183 - t * (183 - 75))
-        b = int(235 - t * (235 - 74))
-        color = f"rgb({r}, {g}, {b})"
-
-        (fig.add_trace
-            (go.Scatter(
-            x=years[i:i + 2],
-            y=temps[i:i + 2],
-            mode="lines",
-            line=dict(color=color, width=3),
-            showlegend=False,
-            hoverinfo="skip"
-        )))
-
-    (fig.add_trace
-        (go.Scatter(
-        x=years,
-        y=temps,
-        mode="markers",
-        marker=dict(size=0.1, color="rgb(0,0,0,0)"),
-        showlegend=False,
-        hovertemplate="%{x}: %{y:.2f°C<extra></extra>}"
-
-    )))
-
-    fig.update_layout(
-        title=f"Temperature change in {name} from 1940 to 2026",
-        xaxis_title="Year",
-        yaxis_title="Temp anomally (°C)",
-        plot_bgcolor="lightgrey",
-        paper_bgcolor="white"
-    )
-
-
-    return fig
-
-graph1()
+    country_choosen = st.selectbox("Country", ["Select any option"] + one_country_name(clean_world_data))
+    if country_choosen != "Select any option":
+        fig = graph1(answer, clean_world_data[clean_world_data["Entity"] == country_choosen], country_choosen)
+        fig.update_layout(
+            margin=dict(l=50, r=20, t=40, b=40),
+            autosize=True
+        )
+        fig.update_xaxes(tickangle=45)
+        st.plotly_chart(fig, use_container_width=True)
