4ca6c1a61885095db6792086bb0535a2456927b9
Shehma Ali <shehmaali08@gmail.com>
2026-07-12 08:28:07 +0500

    style: created sections and updated theme

--- pages/graph1.py
+++ pages/graph1.py
@@ -6,6 +6,16 @@ world_data, all_cities = load_data()
 Allcities, clean_world_data = clean_csvs(all_cities, world_data)
 
 
+st.set_page_config("Temperature Trends", "📈", layout="wide")
+st.title("📈 Temperature Trends", text_alignment= "center")
+st.markdown("<p style='color:#a8ffde;'>Explore the temperatures changes over 80+ years for any country or city individually. </p>",
+            unsafe_allow_html=True, text_alignment="center"
+
+        )
+
+st.divider()
+
+
 def graph1(type, csv, name):
     if type == "City":
         years = csv["YEAR"]
@@ -16,21 +26,40 @@ def graph1(type, csv, name):
         temps = csv["Temperature anomaly"]
         final_title = f"Temperature anomalies in {name}"
 
-    fig = px.line(csv, x=years, y=temps, title= final_title)
+    fig = px.line(csv, x=years, y=temps, title= final_title , color_discrete_sequence= ["orangered"])
 
     return fig
 
+col1, col2 = st.columns([1.5, 1])
 
-st.title("Welcome to Graph 1 :party:")
+with col1:
+    with st.container(border= True):
+        st.markdown(
+            "<h2 style='color:#a6ffe6;'> Start by selecting a category </h2>",
+            unsafe_allow_html=True, text_alignment="center"
+            )
+        with st.container(border= True):
+            choices = ["City", "Country"]
+            answer = st.radio("Choose:                           ", choices)
 
-st.markdown("## Choose the following")
+with col2:
+    with st.container(border= True):
+        st.markdown("### What is temperature anomaly??")
+        st.markdown("<p style='color:#9afcd7;'> A temperature anomaly shows how much warmer or cooler a place is compared to its usual long-term average temperature.</p>",
+            unsafe_allow_html=True, text_alignment="center"
+        )
+        st.markdown("🟥 Positive (+) = Warmer than average")
+        st.markdown("🟦 Negative (−) = Cooler than average")
 
-choices = ["City", "Country"]
 
-answer = st.radio("Choose: ", choices)
+st.divider()
 
 if answer == "City":
     city_choosen = st.selectbox("City", ["Select any option"] + list(Allcities.keys()))
+
+    st.divider()
+
+
     if city_choosen != "Select any option":
         fig = graph1(answer, all_cities[city_choosen], city_choosen)
         fig.update_layout(
@@ -43,6 +72,9 @@ if answer == "City":
 if answer == "Country":
 
     country_choosen = st.selectbox("Country", ["Select any option"] + one_country_name(clean_world_data))
+
+    st.divider()
+
     if country_choosen != "Select any option":
         fig = graph1(answer, clean_world_data[clean_world_data["Entity"] == country_choosen], country_choosen)
         fig.update_layout(
