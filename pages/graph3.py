a35b3d8455ed5515f5512bda06fa12a9d45f4f55
Shehma Ali <shehmaali08@gmail.com>
2026-07-15 07:29:38 +0500

    style: UI of graph 3 is done

--- pages/graph3.py
+++ pages/graph3.py
@@ -10,16 +10,13 @@ all_heatmap_cities = heatmap_cities(Allcities)
 cities_in_anomalies = to_anomalies(all_heatmap_cities)
 city_ranks_df, country_ranks_df = get_ranks(cities_in_anomalies, clean_world_data, one_country_name(clean_world_data))
 
-st.title("Welcome to Graph 3")
+st.set_page_config("Heat Ranking", "🌎", layout="wide")
+st.title("🌎 Heat Ranking", text_alignment="center")
+st.markdown("<p style='color:#9ae7fc;'>Who is heating up the most?? Who warmed up the most?? See the top 20 countries and cities in seconds!!</p>",
+            unsafe_allow_html=True, text_alignment="center"
+            )
 
-st.markdown("## Choose the following")
-
-type_choices = ["City", "Country"]
-warming_choices = ["Unsorted", "Highest warming", "Lowest warming"]
-
-
-category = st.radio("Choose: ", type_choices)
-temp_change = st.radio("Sort by ⬇️", warming_choices)
+st.divider()
 
 def graph3(type, csv, order):
     if type == "City":
@@ -37,12 +34,12 @@ def graph3(type, csv, order):
         if order == "Highest warming":
             csv = csv.sort_values(
                 by="Increase",
-                ascending=False
+                ascending=True
             )
         if order == "Lowest warming":
             csv = csv.sort_values(
                 by="Increase",
-                ascending=True
+                ascending=False
             )
 
     csv = csv.iloc[:15]
@@ -70,9 +67,41 @@ def graph3(type, csv, order):
 
     return fig
 
+
+col1, col2 = st.columns([1.5,1])
+
+with col1:
+    with st.container(border=True, height=365):
+        st.markdown("### Start by selecting a category:", text_alignment="center")
+        st.write(" ")
+
+        type_choices = ["City", "Country"]
+        warming_choices = ["Unsorted", "Highest warming", "Lowest warming"]
+
+        with st.container(border= True, height=120):
+            category = st.radio("Choose: ", type_choices)
+
+        with st.container(border=True, height=125):
+            temp_change = st.radio("Sort by ⬇️", warming_choices)
+
+with col2:
+    with st.container(border= True, height=265):
+        st.markdown("### What is Temperature Anomaly??")
+        st.markdown(
+            "<p style='color:#c4f2ff;'>A temperature anomaly shows how much warmer or cooler a place is compared to its usual long-term average temperature.</p>",
+            unsafe_allow_html=True, text_alignment="center"
+        )
+        st.markdown("🟥 Positive (+) = Warmer than average")
+        st.markdown("🟦 Negative (−) = Cooler than average")
+
+
+st.divider()
+
 if category == "City":
     fig = graph3(category, city_ranks_df, temp_change)
     st.plotly_chart(fig)
+    st.divider()
 if category == "Country":
     fig = graph3(category, country_ranks_df, temp_change)
     st.plotly_chart(fig)
+    st.divider()
